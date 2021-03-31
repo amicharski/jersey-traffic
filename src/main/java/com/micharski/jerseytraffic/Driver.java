1ae6af63afc3f3c4fd6e9a5617a005227395f320
Alex Micharski <amicharski@outlook.com>
2021-03-31 12:58:26 -0400

    initial commit

--- src/main/java/com/micharski/jerseytraffic/Driver.java
+++ src/main/java/com/micharski/jerseytraffic/Driver.java
@@ -1,8 +1,6 @@
 package com.micharski.jerseytraffic;
 
-import com.google.common.collect.MinMaxPriorityQueue;
 import javafx.application.Application;
-import javafx.collections.transformation.SortedList;
 import javafx.geometry.Point2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -10,13 +8,9 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
-import org.apache.commons.collections4.list.FixedSizeList;
-import org.apache.commons.collections4.list.SetUniqueList;
-import org.apache.commons.collections4.set.ListOrderedSet;
 
 import java.util.*;
 import java.util.function.BiFunction;
@@ -30,6 +24,9 @@ public class Driver extends Application {
     static int height = 600;
     static int pointQuantity = 4;
     Stage primaryStage;
+
+    BiFunction<Point2D, Point2D, Double> slope = (a, b) -> (b.getY() - a.getY())/(b.getX() - a.getX());
+    BiFunction<Point2D, Point2D, Integer> slopeY = (a, b) -> (int) (b.getY() - a.getY());
 
     KeyCombination regen = new KeyCodeCombination(KeyCode.ENTER);
 
@@ -56,7 +53,6 @@ public class Driver extends Application {
             generatePoints(root);
             Scene scene = new Scene(root, width+(MARGIN_WIDTH*2), height+(MARGIN_WIDTH*2));
             drawRandomLines(root);
-            //createConvexHull(root, primaryStage, scene);
             setStage(primaryStage, root, scene);
         } catch (Exception ex){
             ex.printStackTrace();
@@ -80,94 +76,113 @@ public class Driver extends Application {
         markIntersections(root);
     }
 
-    //TODO: marks intersections of two lines
+    //TODO: FIX THIS
+    //Bentley-Ottman Algorithm
     public void markIntersections(Group root){
-        PriorityQueue<Point2D> eventPoints = new PriorityQueue<>();
-        Line sweepLine = new Line(5, 0, 5, width+MARGIN_WIDTH);
+        PriorityQueue<Point2D> eventPoints = new PriorityQueue<>(points.size() * 2, Comparator.comparingDouble(Point2D::getX));
+        TreeMap<Point2D, Point2D> crossers = new TreeMap<>(Comparator.comparingDouble(Point2D::getY));
+        Line sweepLine = new Line(5, 0, 5, width+MARGIN_WIDTH*2);
         sweepLine.setStroke(Color.ORANGE);
         root.getChildren().add(sweepLine);
-        //sweepLine.
-    }
-
-    public void createConvexHull(Group root, Stage stage, Scene scene){
-        ListOrderedSet<Point2D> CH = convexHull(root, stage, scene);
-        int avgX = 0;
-        int avgY = 0;
-        for(Point2D p : CH){
-            avgX += p.getX();
-            avgY += p.getY();
-        }
-        avgX /= CH.size();
-        avgY /= CH.size();
-        Point2D center = new Point2D(avgX, avgY);
-        List<Point2D> newCH = new ArrayList<>();
-        newCH.addAll(CH);
-        newCH.sort((Comparator) (o1, o2) -> {
-            Point2D a = (Point2D) o1;
-            Point2D b = (Point2D) o2;
-            if(a.getX() - center.getX() >= 0 && b.getX() - center.getX() < 0){
-                return -1;
-            }
-            if(a.getX() - center.getX() < 0 && b.getX() - center.getX() >= 0){
-                return 1;
-            }
-            return (int) ((a.getX() - center.getX()) * (b.getY() - center.getY()) - (b.getX() - center.getX()) * (a.getY() - center.getY()));
-        });
-        //root.getChildren().add(new Circle(avgX, avgY, 5, Color.RED));
-        for(int i = 0; i < newCH.size(); i++){
-            if(i+1 == newCH.size()){
-                root.getChildren().add(new Line(newCH.get(i).getX(), newCH.get(i).getY(), newCH.get(0).getX(), newCH.get(0).getY()));
-            } else {
-                root.getChildren().add(new Line(newCH.get(i).getX(), newCH.get(i).getY(), newCH.get(i+1).getX(), newCH.get(i+1).getY()));
-            }
-        }
-    }
-
-    public ListOrderedSet<Point2D> convexHull(Group root, Stage stage, Scene scene) {
-        ListOrderedSet returnValue = new ListOrderedSet();
-        for(int i = 0; i < points.size(); i++){
-            if(i == 0){
-                root.getChildren().add(new Circle(points.get(0).getX(), points.get(0).getY(), 5, Color.ORANGE));
-            }
-            for(int j = i+1; j < points.size(); j++){
-                if(j == 1){
-                    root.getChildren().add(new Circle(points.get(1).getX(), points.get(1).getY(), 5, Color.BLUEVIOLET));
-                }
-                boolean valid = true;
-                for(int k = 0; k < points.size(); k++){
-                    if(i != j && j != k && i != k){
-                        if(crossProduct(lineToVector(new Line(points.get(i).getX(), points.get(i).getY(), points.get(j).getX(), points.get(j).getY()), false),
-                                lineToVector(new Line(points.get(i).getX(), points.get(i).getY(), points.get(k).getX(), points.get(k).getY()), false)) >= 0){
-                            valid = false;
-                            break;
-                        }
+        eventPoints.addAll(points);
+        for(int x = (int) eventPoints.peek().getX(); x < width + MARGIN_WIDTH*2; x++){
+            final Point2D p = eventPoints.peek();
+            lines.forEach(l -> {
+                //check if p is the left endpoint of l, then add it to the treemap
+                if(l.getStartX() < l.getEndX()){
+                    if(p.getX() == l.getStartX() && p.getY() == l.getStartY()){
+                        crossers.put(new Point2D(l.getStartX(), l.getStartY()), new Point2D(l.getEndX(), l.getEndY()));
+                    }
+                } else {
+                    if(p.getX() == l.getEndX() && p.getY() == l.getEndY()){
+                        crossers.put(new Point2D(l.getEndX(), l.getEndY()), new Point2D(l.getStartX(), l.getStartY()));
                     }
                 }
-                if(valid){
-                    returnValue.add(points.get(i));
-                    returnValue.add(points.get(j));
-                    //root.getChildren().add(new Line(points.get(i).getX(), points.get(i).getY(), points.get(j).getX(), points.get(j).getY()));
+                //check if p is the right endpoint of l, then remove it from the treemap
+                if(l.getStartX() > l.getEndX()){
+                    if(p.getX() == l.getStartX() && p.getY() == l.getStartY()){
+                        crossers.remove(new Point2D(l.getStartX(), l.getStartY()), new Point2D(l.getEndX(), l.getEndY()));
+                        eventPoints.remove(eventPoints.peek());
+                    }
+                } else {
+                    if(p.getX() == l.getEndX() && p.getY() == l.getEndY()){
+                        crossers.remove(new Point2D(l.getEndX(), l.getEndY()), new Point2D(l.getStartX(), l.getStartY()));
+                        eventPoints.remove(eventPoints.peek());
+                    }
+                }
+                //update each and every value in crossers
+                crossers.forEach((q, r) -> {
+                    if(l.getStartX() < l.getEndX()){
+                        if(p.getX() == l.getStartX() && p.getY() == l.getStartY()){
+                            q.add(1, slope.apply(new Point2D(l.getStartX(), l.getStartY()), new Point2D(l.getEndX(), l.getEndY())) * slopeY.apply(new Point2D(l.getStartX(), l.getStartY()), new Point2D(l.getEndX(), l.getEndY())));
+                        }
+                    } else {
+                        if(p.getX() == l.getEndX() && p.getY() == l.getEndY()){
+                            q.add(1, slopeY.apply(new Point2D(l.getEndX(), l.getEndY()), new Point2D(l.getStartX(), l.getStartY())) * slopeY.apply(new Point2D(l.getEndX(), l.getEndY()), new Point2D(l.getStartX(), l.getStartY())));
+                        }
+                    }
+                });
+            });
+            Set<Object> newPoints = new HashSet<>();
+            for(Point2D q : crossers.values()){
+                if(!newPoints.add(q)){
+                    System.out.println("Point of Intersection: " + q);
                 }
             }
         }
-        return returnValue;
     }
 
-    public Vector<Double> lineToVector(Line line, boolean abs){
-        Vector<Double> returnValue = new Vector<Double>(2, 0);
-        if(abs){
-            returnValue.add(Math.abs(line.getStartX() - line.getEndX()));
-            returnValue.add(Math.abs(line.getStartY() - line.getEndY()));
-        } else {
-            returnValue.add(line.getStartX() - line.getEndX());
-            returnValue.add(line.getStartY() - line.getEndY());
-        }
-        return returnValue;
-    }
-
-    public double crossProduct(Vector<Double> vectorA, Vector<Double> vectorB){
-        return (vectorA.get(0)*vectorB.get(1))-(vectorA.get(1)*vectorB.get(0));
-    }
+//    public void markIntersections(Group root){
+//        ArrayList<Double> slopes = new ArrayList<>();
+//        ArrayList<Line> activeLines = new ArrayList<>();
+//        ArrayList<Point2D> currentPoint = new ArrayList<>();
+//        int min = width + MARGIN_WIDTH*2;
+//        for(Line l : lines) {
+//            double s;
+//            if(l.getStartX() < l.getEndX()){
+//                s = slope.apply(new Point2D(l.getStartX(), l.getStartY()), new Point2D(l.getEndX(), l.getEndY()));
+//                if(l.getStartX() < min){
+//                    min = (int) l.getStartX();
+//                }
+//            } else {
+//                s = slope.apply(new Point2D(l.getEndX(), l.getEndY()), new Point2D(l.getStartX(), l.getStartY()));
+//                if(l.getEndX() < min){
+//                    min = (int) l.getEndX();
+//                }
+//            }
+//            slopes.add(s);
+//        };
+//        Line sweepLine = new Line(min, 0, min, width+MARGIN_WIDTH*2);
+//        sweepLine.setStroke(Color.ORANGE);
+//        root.getChildren().add(sweepLine);
+//        for (int i = (int) sweepLine.getStartX(); i < height + MARGIN_WIDTH*2 - 5; i++) {
+//            sweepLine.setStartX(i);
+//            sweepLine.setEndX(i);
+//            final int x = i;
+//            lines.forEach(l -> {
+//                if(l.getStartX() < l.getEndX()){
+//                    if(l.getStartX() == x){
+//                        activeLines.add(l);
+//                    }
+//                } else {
+//                    if(l.getEndX() == x){
+//                        activeLines.add(l);
+//                    }
+//                }
+//                if(x > l.getStartX() && x > l.getEndX()){
+//                    activeLines.remove(l);
+//                }
+//            });
+//            for(int j = 0; j < )
+//            activeLines.forEach(l -> {
+//                if(l.getStartX() < l.getEndX()){
+//
+//                } else {
+//
+//                }
+//            });
+//        }
+//    }
 
     Runnable regenScene = () -> {
             start(primaryStage); //fix this later
